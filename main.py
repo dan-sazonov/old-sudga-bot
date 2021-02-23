78916f16dae05952616f7c0f45b299b8111bca50
Daniel Sazonov <p.294803@gmail.com>
2021-02-23 18:52:19 +0300

    Some fixes

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import config
-import api
+try:
+    import api
+except ModuleNotFoundError:
+    print('api.py not found. Create this file as described in the readme.')
+    exit(0)
+
 
 print(config.version)
 print(api.API_TOKEN)
